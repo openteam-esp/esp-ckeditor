dd61b887760b1428afe3f650533c3c63a60deee4
Evgeny Lapin <lev@openteam.ru>
2012-02-14 09:35:21 +0700

    add assets to precompile

--- lib/esp-ckeditor.rb
+++ lib/esp-ckeditor.rb
@@ -1,4 +1,17 @@
-require "esp-ckeditor/engine"
+require 'esp-ckeditor/engine'
+require 'pathname'
 
 module EspCkeditor
+
+  def self.root_path
+    @root_path ||= Pathname.new( File.dirname(File.expand_path('../', __FILE__)) )
+  end
+
+  def self.assets
+    Dir[root_path.join('app/assets/javascripts/esp-ckeditor/**', '*.{js,css}')].inject([]) do |list, path|
+      list << Pathname.new(path).relative_path_from(root_path.join('vendor/assets/javascripts'))
+      list
+    end
+  end
+
 end
--- lib/esp-ckeditor/engine.rb
+++ lib/esp-ckeditor/engine.rb
@@ -1,5 +1,9 @@
 module EspCkeditor
   class Engine < Rails::Engine
     isolate_namespace EspCkeditor
+
+    initializer "esp-ckeditor.assets_precompile" do |app|
+      app.config.assets.precompile += EspCkeditor.assets
+    end
   end
 end
